25cb903daad54c25f937845b5f9038ed7bf9e0dc
Optimus9689 <138998195+Optimus9689@users.noreply.github.com>
2024-01-02 10:43:53 +0530

    d1

--- week-2/02-nodejs/solutions/todoServer.solution.file.js
+++ week-2/02-nodejs/solutions/todoServer.solution.file.js
@@ -107,3 +107,5 @@ app.use((req, res, next) => {
 });
 
 module.exports = app;
+
+
--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -11,17 +11,38 @@ const app = express();
 // You have been given a numberOfRequestsForUser object to start off with which
 // clears every one second
 
-let numberOfRequestsForUser = {};
-setInterval(() => {
-    numberOfRequestsForUser = {};
-}, 1000)
 
-app.get('/user', function(req, res) {
+
+let numberOfRequestsForUser = {};
+
+setInterval(() => {
+  numberOfRequestsForUser = {};
+}, 1000);
+
+app.use((req, res, next) => {
+  const userId = req.headers['user-id'];
+
+  // Initialize the request count for the user
+  numberOfRequestsForUser[userId] = numberOfRequestsForUser[userId] || 0;
+
+  if (numberOfRequestsForUser[userId] > 5) {
+    return res.status(404).send('Request limit exceeded');
+  }
+  
+    numberOfRequestsForUser[userId]++;
+    next();
+
+});
+
+app.get('/user', function (req, res) {
   res.status(200).json({ name: 'john' });
 });
 
-app.post('/user', function(req, res) {
+app.post('/user', function (req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
+
+
+
 
 module.exports = app;
